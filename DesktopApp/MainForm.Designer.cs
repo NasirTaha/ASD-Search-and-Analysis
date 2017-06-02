38e9956c37b515b8a271200ca00988f7a39f8b74
Tanya Gupta <tanya.gupta@student.fh-kiel.de>
2017-06-02 12:57:20 +0200

    Merged code

--- DesktopApp/MainForm.Designer.cs
+++ DesktopApp/MainForm.Designer.cs
@@ -28,9 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             this.btnSearch = new System.Windows.Forms.Button();
             this.txtKeyword = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnSearch
@@ -59,17 +66,64 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "Keyword";
             // 
+            // chart1
+            // 
+            this.chart1.AllowDrop = true;
+            chartArea1.AlignmentOrientation = ((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations)((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Vertical | System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Horizontal)));
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(199, 153);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(315, 300);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            this.chart1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.chart1_MouseMove);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(336, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(608, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(809, 465);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtKeyword);
             this.Controls.Add(this.btnSearch);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -80,6 +134,10 @@
         private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.TextBox txtKeyword;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- DesktopApp/MainForm.cs
+++ DesktopApp/MainForm.cs
@@ -10,11 +10,15 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace DesktopApp
 {
     public partial class MainForm : Form
     {
+        string text = null;
+        int positive = 0;
+        int negative = 0;
         public MainForm()
         {
             InitializeComponent();
@@ -53,6 +57,23 @@ namespace DesktopApp
                     //string createText = "Hello and Welcome" + Environment.NewLine;
                     File.WriteAllText(path, contents);
                     MessageBox.Show("Results saved in text file");
+                    this.chart1.Series.Clear();
+                    int positiveresp = getPositiveParameters();
+                    int negativeresp = getNegativeParameters();
+                    chart1.Series.Add("Positive");
+                    chart1.Series["Positive"].ChartType = SeriesChartType.Column;
+                    chart1.Series["Positive"].Points.AddXY(0,positiveresp);
+                    //chart1.Series["Positive"].ChartArea = "ChartArea1";
+                    chart1.Series["Positive"].Color = System.Drawing.Color.Blue;
+                    chart1.Series.Add("Negative");
+                    chart1.Series["Negative"].ChartType = SeriesChartType.Column;
+                    chart1.Series["Negative"].Points.AddXY(1,negativeresp);
+                    //chart1.Series["Negative"].ChartArea = "ChartArea1";
+                    chart1.Series["Negative"].Color = System.Drawing.Color.Red;
+
+
+                    // this.chart1.Series["Positive"].Points.AddXY("Positive", positiveresp);
+                    //this.chart1.Series["Negative"].Points.AddXY("Negative", negativeresp);
 
                 }
 
@@ -62,5 +83,178 @@ namespace DesktopApp
                 MessageBox.Show(ex.Message);
             }
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point mousePoint = new Point(e.X, e.Y);
+            chart1.ChartAreas[0].CursorX.Interval = 0;
+            chart1.ChartAreas[0].CursorY.Interval = 0;
+
+            chart1.ChartAreas[0].CursorX.SetCursorPixelPosition(mousePoint, true);
+            chart1.ChartAreas[0].CursorY.SetCursorPixelPosition(mousePoint, true);
+
+            label2.Text = "Pixel X position : " + chart1.ChartAreas[0].AxisX.PixelPositionToValue(e.X).ToString();
+            label3.Text = "Pixel Y position : " + chart1.ChartAreas[0].AxisY.PixelPositionToValue(e.Y).ToString();
+
+            HitTestResult result = chart1.HitTest(e.X, e.Y);
+            if (result.PointIndex > -1 && result.ChartArea != null)
+            {
+                label4.Text = "Y - value : " + result.Series.Points[result.PointIndex].YValues[0].ToString();
+
+
+            }
+        }
+        private int getPositiveParameters()
+        {
+            positive = 0;
+            text = clearTextGenrator();
+            List<string> positiveList = createPositiveWordList();
+
+            foreach (string positiveElement in positiveList)
+            {
+                if (text.Contains(positiveElement))
+                {
+                    positive++;
+                }
+            }
+            return positive;
+        }
+        private int getNegativeParameters()
+        {
+            negative = 0;
+            text = clearTextGenrator();
+            List<string> negativeList = createNegativeWordList();
+
+            foreach (string negativeElement in negativeList)
+            {
+                if (text.Contains(negativeElement))
+                {
+                    negative++;
+                }
+            }
+            return negative;
+        }
+
+        private List<string> createPositiveWordList()
+        {
+            string path = Environment.CurrentDirectory + "../../../positive.txt";
+            string positiveText = System.IO.File.ReadAllText(path);
+
+            string[] positiveArray = positiveText.Split(',');//<string1/string2/string3/--->     
+            List<string> positiveList = new List<string>(); //make a new string list    
+            positiveList.AddRange(positiveArray);
+            return positiveList;
+        }
+        private List<string> createNegativeWordList()
+        {
+            string path = Environment.CurrentDirectory + "../../../negative.txt";
+            string negativeText = System.IO.File.ReadAllText(path);
+
+            string[] negativeArray = negativeText.Split(',');//<string1/string2/string3/--->     
+            List<string> negativeList = new List<string>(); //make a new string list    
+            negativeList.AddRange(negativeArray);
+            return negativeList;
+
+        }
+
+        private string clearTextGenrator()
+        {
+            string path = Environment.CurrentDirectory + "../../../Results.txt";
+            text = System.IO.File.ReadAllText(path);
+            text = text.ToLower();
+            List<string> clearTextList = getListToClearText();
+            foreach (string element in clearTextList)
+            {
+                if (element == "&")
+                {
+                    text = text.Replace(element, "and");
+                }
+                if (element == ",")
+                {
+                    text = text.Replace(element, " ");
+                }
+                else if (element == "  ")
+                {
+                    text = text.Replace(element, " ");
+                }
+                else if (element == " an ")
+                {
+                    text = text.Replace(element, " ");
+                }
+                else if (element == " from ")
+                {
+                    text = text.Replace(element, " ");
+                }
+                else if (element == " the ")
+                {
+                    text = text.Replace(element, " ");
+                }
+                else if (element == " for ")
+                {
+                    text = text.Replace(element, " ");
+                }
+                else
+                {
+                    text = text.Replace(element, "");
+                }
+
+            }
+
+            text = text.Replace("\"", "");
+            return text;
+        }
+        private List<string> getListToClearText()
+        {
+            List<string> list = new List<string>();
+
+            list.Add("!");
+            list.Add("`");
+            list.Add("~");
+            list.Add("@");
+            list.Add("#");
+            list.Add("$");
+            list.Add("%");
+            list.Add("^");
+            list.Add("&");//extra
+            list.Add("*");
+            list.Add("(");
+            list.Add(")");
+            list.Add("-");
+            list.Add("_");
+            list.Add("+");
+            list.Add("=");
+            list.Add("|");
+            list.Add(@"\");
+            list.Add("}");
+            list.Add("]");
+            list.Add("{");
+            list.Add("[");
+            list.Add("'");
+            list.Add(";");
+            list.Add(":");
+            list.Add("/");
+            list.Add("?");
+            list.Add(">");
+            list.Add("<");
+            list.Add(".");
+            list.Add(",");
+            list.Add("  "); //extra
+            list.Add("‎€");
+            list.Add("‎₹");
+            list.Add(" an ");
+            list.Add(" the ");
+            list.Add(" for ");
+            list.Add(" from ");
+
+            return list;
+
+        }
+
+        
     }
-}
+    }
